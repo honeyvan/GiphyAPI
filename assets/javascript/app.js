65d0f3f6eb228da8a4619f798c4ba2a50c6d59a2
Van Dang <Van Dang>
2017-10-18 20:15:18 -0500

    added styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
 
-  var desserts = ["cake","pie","cupcakes"];
+  var desserts = ["cake","pie","cupcakes","oreos","chocolate pudding","donuts","cookies"];
+  var ajaxResult = null;
   createButtons();
 
   function createButtons() {
 
-        // Deleting the buttons prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#dessertBtns").empty();
+    $("#dessertBtns").empty();
 
-        // Looping through the array of movies
-        for (var i = 0; i < desserts.length; i++) {
-          var a = $("<button>");
-          a.addClass("dessert");
-          a.attr("data-dessert", desserts[i]);
-          a.text(desserts[i]);
-          $("#dessertBtns").append(a);
-        }
-      }
+    for (var i = 0; i < desserts.length; i++) {
+      var a = $("<button>");
+      a.addClass("dessert");
+      a.attr("data-dessert", desserts[i]);
+      a.text(desserts[i]);
+      $("#dessertBtns").append(a);
+    }
+  }
 
   $("#addDessert").on("click", function() {
     event.preventDefault();
@@ -30,7 +28,7 @@ $(document).ready(function() {
   });
 
   $(document).on("click", ".dessert", function() {
-    // Grabbing and storing the data-dessert property value from the button
+
     var dessert = $(this).attr("data-dessert");
 
     if (dessert !== undefined) {
@@ -40,13 +38,13 @@ $(document).ready(function() {
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         dessert + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      // Performing an AJAX request with the queryURL
       $.ajax({
           url: queryURL,
           method: "GET"
       })
         .done(function(response) {
           var results = response.data;
+          ajaxResult = results;
           console.log(response);
           for (var i = 0; i < results.length; i++) {
 
@@ -54,18 +52,15 @@ $(document).ready(function() {
             var dessertDiv = $("<div>");
             dessertDiv.addClass("dessert");
 
-            // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + results[i].rating);
 
             var dessertImage = $("<img>");
     
             dessertImage.attr("src", results[i].images.fixed_height.url);
             dessertImage.attr("stillAnimate","animate");
-            // use fixed_height_still.url for the image still
-            //if data-still whatever = still, change url & animate, vice versa
             dessertImage.addClass("gif");
+            dessertImage.attr("id",i);
 
-            // Appending the paragraph and image tag to the dessertDiv
             dessertDiv.append(p);
             dessertDiv.append(dessertImage);
 
@@ -76,12 +71,13 @@ $(document).ready(function() {
   }); //close click button
 
   $(document).on("click","img", function() {
+    var idVal = $(this).attr("id");
     if ($(this).attr("stillAnimate") == "animate") {
-      $(this).attr("src", results[i].images.fixed_height_still.url); // need to be in ajax to call
+      $(this).attr("src", ajaxResult[idVal].images.fixed_height_still.url); // need to be in ajax to call
       $(this).attr("stillAnimate", "still"); 
     }
     else if ($(this).attr("stillAnimate") == "still") {
-      $(this).attr("src", results[i].images.fixed_height.url);
+      $(this).attr("src", ajaxResult[idVal].images.fixed_height.url);
       $(this).attr("stillAnimate", "animate"); 
     }
   }); //close click img
